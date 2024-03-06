3e50eb6840b3e40920e134074fd9dbcccd6513de
Sergey Kalistratov <rimusolamus@gmail.com>
2024-03-06 15:14:55 +0100

    make year dynamic

--- app/src/main/java/cz/rimu/prodlouzenevikendy/data/RemotePublicHolidayRepository.kt
+++ app/src/main/java/cz/rimu/prodlouzenevikendy/data/RemotePublicHolidayRepository.kt
@@ -3,11 +3,12 @@ package cz.rimu.prodlouzenevikendy.data
 import cz.rimu.prodlouzenevikendy.domain.PublicHolidaysRepository
 import cz.rimu.prodlouzenevikendy.model.Api
 import cz.rimu.prodlouzenevikendy.model.PublicHoliday
+import java.time.LocalDate
 
 class RemotePublicHolidayRepository(
     private val api: Api
 ): PublicHolidaysRepository {
     override suspend fun getPublicHolidays(): List<PublicHoliday> {
-        return api.getPublicHolidays()
+        return api.getPublicHolidays(LocalDate.now().year.toString())
     }
 }
--- app/src/main/java/cz/rimu/prodlouzenevikendy/model/Api.kt
+++ app/src/main/java/cz/rimu/prodlouzenevikendy/model/Api.kt
@@ -1,8 +1,9 @@
 package cz.rimu.prodlouzenevikendy.model
 
 import retrofit2.http.GET
+import retrofit2.http.Path
 
 interface Api {
-    @GET("PublicHolidays/2024/CZ")
-    suspend fun getPublicHolidays(): List<PublicHoliday>
+    @GET("PublicHolidays/{year}/CZ")
+    suspend fun getPublicHolidays(@Path("year") year: String): List<PublicHoliday>
 }
